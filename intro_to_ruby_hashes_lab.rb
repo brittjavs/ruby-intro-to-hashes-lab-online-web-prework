29420d659c531f67a85188675a8ddce24ddcb8a3
Brittany Javalera <brittjavalera@gmail.com>
2019-05-13 23:32:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,10 @@ def monopoly_with_third_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4, 
-	    :names => {:reading_railroad => {},
-	    :pennsylvania_railroad => {}
-	    :b_and_o_railroad => {}
+	    :names => {
+	     :reading_railroad => {},
+	    :pennsylvania_railroad => {},
+	    :b_and_o_railroad => {},
 	    :shoreline => {}
 	    },
 	    :rent_in_dollars => {
